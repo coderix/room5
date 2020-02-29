0e931221d26f1cf26a1827e224fd6283b18012d0
Dirk Neumann <dneumann42@googlemail.com>
2020-02-29 16:57:16 +0100

    room model

---
+++ Room5/Models/IRoomRepository.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Room5.Models
+{
+    interface IRoomRepository
+    {
+        /// <summary>
+        /// Returns all rooms. 
+        /// </summary>
+        Task<IEnumerable<Room>> GetAsync();
+
+        /// <summary>
+        /// Returns all rooms with a data field matching the start of the given string. 
+        /// </summary>
+        Task<IEnumerable<Room>> GetAsync(string search);
+
+        /// <summary>
+        /// Returns the room with the given id. 
+        /// </summary>
+        Task<Room> GetAsync(Guid id);
+
+        /// <summary>
+        /// Adds a new room if the room does not exist, updates the 
+        /// existing room otherwise.
+        /// </summary>
+        Task<Room> UpsertAsync(Room room);
+
+        /// <summary>
+        /// Deletes a room.
+        /// </summary>
+        Task DeleteAsync(Guid roomId);
+    }
+}
+}
