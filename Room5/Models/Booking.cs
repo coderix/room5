e3e2deed8a813a568ee64844f586048dc24e7528
Dirk Neumann <dirkos@hotmail.de>
2020-03-18 14:56:23 +0100

    Booking 1

---
+++ Room5/Models/Booking.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Room5.Models
+{
+    public class Booking : IEquatable<Booking>
+    {
+        public string Title { get; set; }
+
+        public Guid Id { get; set; } = Guid.NewGuid();
+        public bool Equals(Booking other)
+        {
+            return
+                this.Id == other.Id;
+        }
+    }
+}
