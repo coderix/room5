dd03ded43abc68afa2866beef69fa8ab78be64da
Dirk Neumann <dirkos@hotmail.de>
2020-03-31 14:43:27 +0200

    Booking model

--- Room5/Models/Booking.cs
+++ Room5/Models/Booking.cs
@@ -8,11 +8,21 @@ namespace Room5.Models
 {
     public class Booking : IEquatable<Booking>
     {
-        public string Title { get; set; }
+       
 
         public Guid BookingId { get; set; } = Guid.NewGuid();
-        public int RoomId { get; set; }
-        public Room room { get; set; }
+        // public string RoomId { get; set; }
+        public Room Room { get; set; }
+
+        public string Title { get; set; }
+        public int Day { get; set; }
+        public int Lesson { get; set; }
+        public bool IsRecurrent { get; set; }
+        public int Repeat { get; set; }
+        public DateTime StartDate  { get; set; }
+        public DateTime EndDate { get; set; }
+        public string Remarks { get; set; }
+
         public bool Equals(Booking other)
         {
             return
