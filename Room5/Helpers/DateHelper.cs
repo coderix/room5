65b2843a74ae43a58e5b335b8cbb08c8fc197bfa
Dirk Neumann <dneumann42@googlemail.com>
2020-04-12 09:23:22 +0200

    test method

--- Room5/Helpers/DateHelper.cs
+++ Room5/Helpers/DateHelper.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Room5.Helpers
@@ -34,7 +36,55 @@ namespace Room5.Helpers
                 return d;
             }
         }
-      
+
+        static void dates()
+        {
+            DateTime date1 = DateTime.Now;
+            Console.WriteLine(date1.ToString());
+            Console.WriteLine($"{date1.Day}.{date1.Month}.");
+            Console.WriteLine("The day of the week for {0:d} is {1}.", date1, (int)date1.DayOfWeek);
+
+            DateTime dateToDisplay = new DateTime(2009, 6, 1, 8, 42, 50);
+            CultureInfo originalCulture = Thread.CurrentThread.CurrentCulture;
+            // Change culture to de-DE.
+            Thread.CurrentThread.CurrentCulture = new CultureInfo("de-DE");
+            Console.WriteLine("Displaying short date for {0} culture:",
+                              Thread.CurrentThread.CurrentCulture.Name);
+            Console.WriteLine("   {0} (Short Date String)",
+                              dateToDisplay.ToShortDateString());
+            // Display using 'd' standard format specifier to illustrate it is
+            // identical to the string returned by ToShortDateString.
+            Console.WriteLine("   {0} ('d' standard format specifier)",
+                              dateToDisplay.ToString("d"));
+            Console.WriteLine();
+
+
+            // Restore original culture.
+            Thread.CurrentThread.CurrentCulture = originalCulture;
+
+            DateTime today = DateTime.Now;
+            DateTime firstMonday = DateTime.Now;
+            Console.WriteLine("The day of the week for {0} is {1}.", today.DayOfWeek, (int)today.DayOfWeek);
+
+            //Get the date of this week's monday
+            switch ((int)today.DayOfWeek)
+            {
+
+                case 0: firstMonday = today.AddDays(-6); break; //Sunday
+                case 1: firstMonday = today; break; //Monday
+                case 2: firstMonday = today.AddDays(-1); break; // Tuesday
+                case 3: firstMonday = today.AddDays(-2); break;
+                case 4: firstMonday = today.AddDays(-3); break;
+                case 5: firstMonday = today.AddDays(-4); break;
+                case 6: firstMonday = today.AddDays(-5); break;
+                default:
+                    break;
+            }
+         
+            Console.WriteLine("The day of the week for this week's monday {0} is {1},  {2}.", firstMonday.DayOfWeek, (int)firstMonday.DayOfWeek, firstMonday.ToShortDateString());
+
+        }
+
 
     }
 }
